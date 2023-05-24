ee198fba0e737e95f36f211b1beb30ea533fec1b
chamarasab <chamara.sab@outlook.com>
2023-05-24 12:21:01 +0530

    module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StocksModule } from './stocks/stocks.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://chamara:UaZESTbNGfyUAQ4k@shopcluster.th35ivz.mongodb.net/shop_db?retryWrites=true&w=majority'),
+    StocksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
