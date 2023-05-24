a1c89b3c57bbb87cb52b667b682ed4913b85877f
chamarasab <chamara.sab@outlook.com>
2023-05-24 12:45:35 +0530

    controller modified

--- src/stocks/stocks.controller.ts
+++ src/stocks/stocks.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { StocksService } from './stocks.service';
 
 @Controller('stocks')
-export class StocksController {}
+export class StocksController {
+    constructor(private stocksService: StocksService) {
+    }
+
+    @Get()
+    async getAll() {
+        return this.stocksService.getAll();
+    }
+}
