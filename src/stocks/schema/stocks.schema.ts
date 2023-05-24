2698e0baab78c1729c721488f3910721474afd87
chamarasab <chamara.sab@outlook.com>
2023-05-24 12:31:38 +0530

    schema modified

--- src/stocks/schema/stocks.schema.ts
+++ src/stocks/schema/stocks.schema.ts
@@ -1,1 +1,16 @@
-export class StocksSchema {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { type } from "os";
+export type StocksDocument = Stocks & Document;
+@Schema({ collection: 'stocks' })
+export class Stocks {
+    @Prop()
+    name: string;
+
+    @Prop()
+    price: string;
+
+    @Prop()
+    quantity: string;
+}
+
+export const StocksSchema = SchemaFactory.createForClass(Stocks);
