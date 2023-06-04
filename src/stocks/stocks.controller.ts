7a3b36b14f25a965c365e0b0e9cdec197041208d
chamarasab <chamara.sab@outlook.com>
2023-06-04 15:13:56 +0530

    create a function, get id if it exists in the url, pass it to the service function.

--- src/stocks/stocks.controller.ts
+++ src/stocks/stocks.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { StocksService } from './stocks.service';
 import { Stocks } from './schema/stocks.schema';
 
 @Controller('stocks')
 export class StocksController {
-    constructor(private stocksService: StocksService) {
-    }
+  constructor(private stocksService: StocksService) {}
 
-    @Get()
-    async getAll() {
-        return this.stocksService.getAll();
-    }
+  @Get()
+  async getAll() {
+    return this.stocksService.getAll();
+  }
 
-    @Post()
-    async createStocks(@Body() stocks: Stocks) {
-        return this.stocksService.create(stocks);
-    }
+  @Post()
+  async createStocks(@Body() stocks: Stocks) {
+    return this.stocksService.create(stocks);
+  }
+  @Get('/:id')
+  async getById(@Param('id') id: string) {
+    return this.stocksService.getById(id);
+  }
 }
